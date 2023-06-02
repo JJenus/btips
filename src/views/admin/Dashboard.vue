7055e62c271d18b31a6d487a26c5a27dec1e30b4
jjenus <biz.jenus@gmail.com>
2023-06-02 14:14:47 +0100

    added dashboard summary

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,31 +1,73 @@
 <script setup>
+	import { onMounted, ref } from "vue";
 	import axios from "axios";
-	import { inject, onMounted, ref } from "vue";
-	import { user } from "@/stores/user";
+	import { util } from "../../stores/utility.js";
+	import { computed } from "@vue/reactivity";
 
-	const avatar = ref(user.defaultAvatar());
-	const User = inject("user", user.getUser());
-
+	//CODES
 	const env = import.meta.env;
-	const subscriptions = ref([]);
 
-	function openChatApp() {
-		window.tidioChatApi.open();
-	}
+	const users = ref([]);
+	const plans = ref([]);
+	const userSubs = ref([]);
 
-	async function loadSubscriptions() {
+	const totalSubsAmount = computed(() => {
+		return userSubs.value.reduce((p, sub) => {
+			const amount = sub.subscription.amount.replace("$", "");
+			return p + Number(amount);
+		}, 0);
+	});
+
+	async function loadUsers() {
 		let config = {
 			method: "GET",
-			url: `${env.VITE_BE_API}/user-subscriptions/${User.value.id}`,
+			url: `${env.VITE_BE_API}/users/`,
+		};
+
+		axios
+			.request(config)
+			.then((response) => {
+				console.log(response);
+				users.value = response.data.filter((user) => {
+					if (user.roles[0].name !== "ADMIN") return user;
+				});
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+			.finally(() => {});
+	}
+
+	async function loadUserSubs() {
+		let config = {
+			method: "GET",
+			url: `${env.VITE_BE_API}/user-subscriptions`,
+		};
+
+		axios
+			.request(config)
+			.then((response) => {
+				console.log(response);
+				userSubs.value = response.data;
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+			.finally(() => {});
+	}
+
+	async function loadPlans() {
+		let config = {
+			method: "GET",
+			url: `${env.VITE_BE_API}/subscriptions`,
 		};
 
 		axios
 			.request(config)
 			.then((res) => {
-				console.log(res.data);
-				window.debug.log(res);
+				console.log(res);
 				let data = res.data;
-				subscriptions.value = data;
+				plans.value = data;
 			})
 			.catch((error) => {
 				console.log(error);
@@ -33,13 +75,10 @@
 			.finally(() => {});
 	}
 
-	function status(stat) {
-		if (!stat) return "pending";
-		return stat;
-	}
-
 	onMounted(() => {
-		loadSubscriptions();
+		loadPlans();
+		loadUsers();
+		loadUserSubs();
 	});
 </script>
 
@@ -66,7 +105,104 @@
 		<div id="kt_account_settings_overview" class="collapse show">
 			<!--begin::Card body-->
 			<div class="card-body border-top p-9">
-				<span class="text-muted">Empty</span>
+				<div class="row align-items-stretched g-4">
+					<div
+						class="col-xl-6 col-md-6 stretch-card grid-margin grid-margin-sm-0 pb-sm-3"
+					>
+						<div class="card border-gray-300 h-100">
+							<div
+								class="card-body d-block d-flex align-items-center justify-content-between"
+							>
+								<div>
+									<span class="d-block h6 fw-bold"
+										>Users</span
+									>
+									<h5 class="h3 fw-bold mb-1">
+										{{ users.length }}
+									</h5>
+								</div>
+								<div
+									class="icon icon-shape icon-md icon-shape-primary rounded-circle me-3 mb-4 mb-md-0"
+								>
+									<i class="bi display-6 bi-people-fill"></i>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div
+						class="col-xl-6 col-md-6 stretch-card grid-margin grid-margin-sm-0 pb-sm-3"
+					>
+						<div class="card border-gray-300 h-100">
+							<div
+								class="card-body d-block d-flex align-items-center justify-content-between"
+							>
+								<div>
+									<span class="d-block h6 fw-bold">
+										Subscription Plans
+									</span>
+									<h5 class="h3 fw-bold mb-1">
+										{{ plans.length }}
+									</h5>
+								</div>
+								<div
+									class="icon icon-shape icon-md icon-shape-primary rounded-circle me-3 mb-4 mb-md-0"
+								>
+									<i
+										class="bi display-6 bi-card-checklist"
+									></i>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-xl-6 col-md-6 stretch-card pb-sm-3 pb-lg-0">
+						<div class="card border-gray-300 h-100">
+							<div
+								class="card-body d-block d-flex align-items-center justify-content-between"
+							>
+								<div>
+									<span class="d-block h6 fw-bold">
+										Total Subscriptions
+									</span>
+									<h5 class="h3 fw-bold mb-1">
+										{{ userSubs.length }}
+									</h5>
+								</div>
+								<div
+									class="icon icon-shape icon-md icon-shape-primary rounded-circle me-3 mb-4 mb-md-0"
+								>
+									<i
+										class="bi display-6 bi-card-checklist"
+									></i>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div
+						class="col-xl-6 col-md-6 stretch-card grid-margin grid-margin-sm-0 pb-sm-3 pb-lg-0 pb-xl-3"
+					>
+						<div class="card border-gray-300 h-100">
+							<div
+								class="card-body d-block d-flex align-items-center justify-content-between"
+							>
+								<div>
+									<span class="d-block h6 fw-bold">
+										Subscribed Amount
+									</span>
+									<h5 class="h3 fw-bold mb-1">
+										${{ totalSubsAmount }}
+									</h5>
+								</div>
+								<div
+									class="icon icon-shape icon-md icon-shape-primary rounded-circle me-3 mb-4 mb-md-0"
+								>
+									<i class="fa-solid fa-coins display-6"></i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<!--end::Card body-->
 		</div>
